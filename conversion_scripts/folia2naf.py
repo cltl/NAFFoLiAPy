5d2c11f0237f3f78e51fdbd53819373f22da3a5e
antske <antske.fokkens@vu.nl>
2016-08-20 21:30:11 +0200

    Added basic functions for chunks and entities

--- conversion_scripts/folia2naf.py
+++ conversion_scripts/folia2naf.py
@@ -9,6 +9,8 @@ from pynlpl.formats import folia
 import sys
 
 
+#global dictionary that maps folia token ids to NAF term ids
+fid2tid = {}
 
 
 def header_to_header_layer(foliaObj, nafObj):
@@ -77,12 +79,6 @@ def set_folia_info(folia_word, term):
     naf_pos = folia_word.xml().find('{http://ilk.uvt.nl/folia}pos').get('head')
     term.set_pos(naf_pos)
 
-    # TODO use pos to derive type (open or closed)
-    # w; class -> no place in NAF
-    # pos; confidence -> not in NAF at this level
-    # lemma; class -> lemma
-    # morphology -> no place in NAF
-
 
 def get_and_add_term_information(folia_word, word_count):
     '''
@@ -92,9 +88,12 @@ def get_and_add_term_information(folia_word, word_count):
     :param word_count: count for term id/span
     :return: None
     '''
+    global fid2tid
     naf_term = Cterm()
     # adding obligatory elements
-    naf_term.set_id('t' + str(word_count))
+    term_id = 't' + str(word_count)
+    naf_term.set_id(term_id)
+    fid2tid[folia_word.id] = term_id
     naf_span = create_span(['w' + str(word_count)])
     naf_term.set_span(naf_span)
     # add information from foliaWord
@@ -104,8 +103,8 @@ def get_and_add_term_information(folia_word, word_count):
 def text_to_text_layer(folia_obj, naf_obj):
     '''
     Goes through folia's text and adds all tokens to NAF token layer
-    :param foliaobj: folia input object
-    :param nafobj: naf output object
+    :param folia_obj: folia input object
+    :param naf_obj: naf output object
     :return: None
     '''
     # FoLiA does not provide offset, length; setting it ourselves
@@ -132,16 +131,16 @@ def text_to_text_layer(folia_obj, naf_obj):
             naf_sent += 1
 
 
-def add_raw_from_text_layer(nafObj):
+def add_raw_from_text_layer(naf_obj):
     '''
     Goes through NAF's token layer and adds a raw layer based on its data.
-    :param nafobj: nafobject containing text layer
+    :param naf_obj: nafobject containing text layer
     :return: None
     '''
     raw = ''
     offset = 0
     paragraph = '1'
-    for tok in nafObj.get_tokens():
+    for tok in naf_obj.get_tokens():
         # add space and update offset if there was a space
         if tok.get_offset() != str(offset):
             raw += ' '
@@ -153,15 +152,82 @@ def add_raw_from_text_layer(nafObj):
         token = tok.get_text()
         raw += token
         offset += len(token)
-    nafObj.set_raw(raw)
+    naf_obj.set_raw(raw)
 
 
-def check_overall_info(foliaObj):
+def create_span_from_folia_words(folia_word_list):
     '''
-    :param foliaObj:
+    Goes through list of folia words and identifies corresponding term id for each
+    :param folia_word_list: list of FoLiA word objects
+    :return: list of term ids
+    '''
+
+    global fid2tid
+    naf_span = []
+    for word in folia_word_list:
+        naf_term_id = fid2tid.get(word.id)
+        naf_span.append(naf_term_id)
+    return naf_span
+
+
+def add_span_to_elem(naf_elem, span_ids):
+    '''
+    Creates a NAF span object from a list of ids and adds this to the naf element
+    :param naf_elem: a naf element
+    :param span_ids: a list of ids that composes the span
+    :return: None
+    '''
+
+    span = Cspan()
+    span.create_from_ids(span_ids)
+    naf_elem.set_span(span)
+
+def chunking_to_chunks_layer(folia_obj, naf_obj):
+    '''
+    Extract chunks from FoLiA object and add to NAF's chunk layer
+    :param folia_obj: folia object
+    :param naf_obj: naf object
+    :return: None
+    '''
+    chunk_id = 1
+    for chunk in folia_obj.select(folia.Chunk):
+        naf_chunk = Cchunk()
+        naf_chunk.set_id('c' + str(chunk_id))
+        chunk_id += 1
+        naf_span = create_span_from_folia_words(chunk.wrefs())
+        add_span_to_elem(naf_chunk, naf_span)
+        naf_chunk.set_phrase(chunk.cls)
+        #add phrase, head (when possible)
+        if len(naf_span) == 1:
+            naf_chunk.set_head(naf_span[0])
+        naf_obj.add_chunk(naf_chunk)
+
+
+def entities_to_entity_layer(folia_obj, naf_obj):
+    '''
+    Retrieves all entities from folia obj and adds them to naf entity layer
+    :param folia_obj: folia object
+    :param naf_obj: naf object
+    :return: None
+    '''
+    entity_id = 1
+    for entity in folia_obj.select(folia.Entity):
+        naf_entity = Centity()
+        naf_entity.set_id('e' + str(entity_id))
+        entity_id += 1
+        naf_span = create_span_from_folia_words(entity.wrefs())
+        entity_references = Creferences()
+        add_span_to_elem(entity_references, naf_span)
+        naf_entity.add_reference(entity_references)
+        naf_entity.set_type(entity.cls)
+        naf_obj.add_entity(naf_entity)
+
+def check_overall_info(folia_obj):
+    '''
+    :param folia_obj:
     :return:
     '''
-    if foliaObj.version is None:
+    if folia_obj.version is None:
         print('[WARNING] FoLiA input did not have a version indicated.', file=sys.stderr)
 
         # print('Problems')
@@ -177,19 +243,19 @@ def convert_file_to_naf(inputfolia, outputnaf=None):
     if outputnaf == None:
         outputnaf = "".join([inputfolia, '.naf'])
 
-    foliaObj = folia.Document(file=inputfolia)
-    check_overall_info(foliaObj)
+    folia_obj = folia.Document(file=inputfolia)
+    check_overall_info(folia_obj)
     # check what information is present and print warnings if not all can be handled (yet)
 
 
-    nafObj = KafNafParser(type='NAF')
-    text_to_text_layer(foliaObj, nafObj)
-    add_raw_from_text_layer(nafObj)
-    nafObj.dump(outputnaf)
+    naf_obj = KafNafParser(type='NAF')
+    text_to_text_layer(folia_obj, naf_obj)
+    add_raw_from_text_layer(naf_obj)
+    chunking_to_chunks_layer(folia_obj, naf_obj)
+    entities_to_entity_layer(folia_obj, naf_obj)
+    naf_obj.dump(outputnaf)
 
-    header_to_header_layer(foliaObj, nafObj)
-    # print(foliaObj.version)
-
+    header_to_header_layer(folia_obj, naf_obj)
 
 def main(argv=None):
     # option to add: keep original identifiers...
